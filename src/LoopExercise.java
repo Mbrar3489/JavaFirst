8996a1c52a8118d57b3ef5562446aaf4f0be08f0
Mbrar3489 <manpreetbrarit401@gmail.com>
2023-02-11 21:27:12 -0500

    Values Change

--- src/LoopExercise.java
+++ src/LoopExercise.java
@@ -1,7 +1,7 @@
 public class LoopExercise {
      public static void main(String[] args)
      {
-         for(int i=1; i<=5; i++)
+         for(int i=0; i<=5; i++)
          {
              for(int j=i ; j<=5; j++)
              {
